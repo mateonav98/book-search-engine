d592292a9740ce7f64d808629cf87af5ef9694db
mateonav98 <mateonav98@gmail.com>
2022-06-02 16:17:32 -0700

    update typedef in schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,18 +5,34 @@ const typeDefs = gql`
         _id: ID
         username: String
         email: String
-        password: String
+        bookCount: Int
         savedBooks: [Book]
     }
     type Book {
+        bookId: String
         authors: [String]
         description: String
-        bookId: ID
+        title: String
         image: String
         link: String
-        title: String
     }
-
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        users: [User]
+        user(username: String!): User
+        books(username: String): [Book]
+        book(bookID: ID!): Book
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookId: String, authors: [String], description: String, title: String,  image: String, link: String) : User
+        removeBook(userId: ID!, BookId: ID!): Book
+    }
 `
 module.exports = typeDefs;
 
