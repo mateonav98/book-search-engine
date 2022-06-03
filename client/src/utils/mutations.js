8e2df09f5ec7a74d5ac8502be8cff5c1f83ec892
mateonav98 <mateonav98@gmail.com>
2022-06-02 18:15:33 -0700

    outline utils mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,13 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+`;
+
+export const ADD_USER = gql`
+`;
+
+export const SAVE_BOOK = gql`
+`;
+
+export const REMOVE_BOOK = gql`
+`;
