d9b6db73af0fe0e054dfc15012fdc4a80e72c963
mateonav98 <mateonav98@gmail.com>
2022-06-02 20:55:24 -0700

    ADD_USER for sign up form

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -3,7 +3,6 @@ import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
